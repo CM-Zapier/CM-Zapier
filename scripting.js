14b01e68d769af666c8de1954ca729274f3b17a5
Marc <marc.j.bouwman@gmail.com>
2018-06-01 20:44:03 +0200

    Input data tests, better error handling, improve list iterations

--- scripting.js
+++ scripting.js
@@ -22,130 +22,172 @@ const {
 // END: HEADER -- AUTOMATICALLY ADDED FOR COMPATIBILITY - v1.2.0
 
 String.prototype.replaceAll = function (search, replacement) {
-    return this.replace(new RegExp(search, 'g'), replacement);
-};
-
-Date.prototype.addMinutes = function (minutes) {
-    return new Date(this.getTime() + minutes*60000);
+    return this.replace(new RegExp(search, 'g'), replacement)
 }
 
-var settings = {
-    useVoiceTokenAuthentication: false, // True if Voice should use token authentication, false if Voice should use username + shared key.
-    debug: false // True to print all errors in json.
-};
+Date.prototype.addMinutes = function (minutes) {
+    return new Date(this.getTime() + minutes * 60 * 1000)
+}
 
-function createRequest(headers, data) {
+Date.prototype.addHours = function (hours) {
+    return new Date(this.getTime() + hours * 60 * 60 * 1000)
+}
+
+var Settings = {
+    useVoiceTokenAuthentication: false, // True if Voice should use token authentication, false if Voice should use username + shared key.
+    validityTime: {
+        def: "48h0m", // Default
+        min: 1,
+        max: 48 * 60
+    },
+    defaultReference: "None",
+    debug: false // True to print all errors in json.
+}
+
+function ZapierRequest(headers, data) {
     return {
         headers: headers,
         data: typeof data == "string" ? data : JSON.stringify(data) // Our data needs to be a string, so when it's JSON convert it.
-    };
+    }
 }
 
-function createMessagesRequestData(authentication, msg) {
+function RequestHeaders(bundle) {
     return {
-        Messages: {
-            Authentication: authentication,
-            Msg: msg
+        'Content-Type': "application/json",
+        'X-CM-PRODUCTTOKEN': bundle.auth_fields.productToken
+    }
+}
+
+var Messages = {
+    getAuthentication: function (bundle) {
+        return {
+            ProductToken: bundle.auth_fields.productToken
         }
-    };
+    },
+
+    getData: function (authentication, messageList) {
+        return {
+            Messages: {
+                Authentication: authentication,
+                Msg: messageList
+            }
+        }
+    }
 }
 
-function createAuthentication(bundle) {
-    return {
-        ProductToken: bundle.auth_fields.productToken
-    };
-}
-
+// Log JSON as readable string.
 function logJSON(json) {
-    console.log(JSON.stringify(json, null, 4));
+    console.log(JSON.stringify(json, null, 4))
 }
 
-function parseValidityTime(input){
-    var validityTime = input;
-    validityTime = validityTime === undefined ? "48h0m" : validityTime;
-    validityTime = validityTime.split("h");
-    validityTime[1] = validityTime[1].replace("m", "");
-    var validityTimeMinutes = parseInt(validityTime[1]) + (parseInt(validityTime[0]) * 60);
-    var currentDate = new Date();
-    var validityTime = currentDate.addMinutes(validityTimeMinutes);
-    validityTime = validityTime.toISOString();
-    validityTime = validityTime.replace("T", " ");
-    validityTime = validityTime.split(".")[0];
-    validityTime += " GMT";
-    return validityTime;
+// Convert the input (like 1h2m) to a date from now (current datetime + 1h2m)
+function parseValidityTime(input) {
+    // If the input is undefined, use default Validity time.
+    var validityTime = input === undefined ? Settings.validityTime.def : input
+
+    if (!validityTime.matches(/[0-9]*h[0-5][0-9]m/)) {
+        throw new ErrorException("Validity time is in an incorrect format")
+    }
+
+    // Convert the input (like 1h2m) to minutes (1 * 60 + 2)
+    validityTime = validityTime.split("h") // Result: ["1", "2m"]
+    validityTime[1] = validityTime[1].replace("m", "")
+
+    // Convert strings in the array to integers
+    validityTime = validityTime.map(function (item) {
+        return parseInt(item)
+    })
+
+    var hours = validityTime[0]
+    var minutes = validityTime[1]
+    var total = hours * 60 + minutes
+
+    if (total > Settings.validityTime.max) {
+        throw new ErrorException("Validity time is larger than allowed.")
+    } else if (total < Settings.validityTime.min) {
+        throw new ErrorException("Validity time is smaller than allowed.")
+    }
+
+    var currentDate = new Date()
+    var validityDate = currentDate.addHours(hours).addMinutes(minutes)
+
+    return validityDate.toISOString().replace("T", " ").split(".")[0] + " GMT" // Convert the date to a date CM.com can read.
 }
 
+// When Zapier gets a result from CM.com, check it for errors and throw it as readable message.
 function throwResponseError(bundle) {
     if (!(bundle.response.status_code >= 200 && bundle.response.status_code < 300)) {
-        var response = JSON.parse(bundle.response.content);
-        var errorMessage = "";
-        try {
-            if (settings.debug) { // Throw json error when debugging.
-                errorMessage = JSON.stringify(response, null, 4);
-            } else if (response.message !== undefined) { // Voice error handling
-                errorMessage = response.message;
-            } else if (response.messages !== undefined) { // Text error handling
-                if (response.messages.length === 0) {
-                    errorMessage = response.details;
-                } else {
-                    for (var i = 0; i < response.messages.length; i++) {
-                        errorMessage += response.messages[i].messageDetails + "\n";
-                    }
-                }
+        var response = JSON.parse(bundle.response.content)
+        var errorMessage
+        if (Settings.debug) { // Throw json error when debugging.
+            errorMessage = JSON.stringify(response, null, 4)
+        } else {
+            var errorMessages = [] // A list of error messages
+            if (response.message !== undefined) { // If the error has a single message.
+                errorMessages.push(response.message) // Add the message to the list
+            } else if (response.messages !== undefined && response.messages.length > 0) { // If the error has multiple messages in an array.
+                errorMessages = response.messages.filter(function (item) {
+                    return item.messageDetails !== null // Filter all success messages from the list
+                }).map(function (item) { // Convert all error messages from json to text.
+                    // Example: Message 2 (to 00447911123457 with reference your_reference_B): A body without content was found (error code 304)
+                    return "Message " + response.messages.indexOf(item) + " (to " + item.to + (item.reference != "None" ? " with reference " + item.reference : "") + ") " +
+                        item.messageDetails + " (error code " + item.errorCode + ")"
+                })
             }
-            if (response.errorCode !== undefined) errorMessage += " (error code: " + response.errorCode + ")";
-        } catch (error) {
-            errorMessage = JSON.stringify(response, null, 4);
+
+            if (errorMessages.length === 0) { // If no error messages where found, use details and errorCode.
+                var msg = ""
+
+                if (response.details !== undefined) {
+                    msg += response.details
+                }
+                if (response.errorCode !== undefined) {
+                    msg += response.details !== undefined ? " (error code: " + response.errorCode + ")" : "Error with code: " + response.errorCode
+                }
+
+                errorMessages.push(msg)
+            }
+
+            errorMessage = errorMessages.join("\n")
         }
-        throw new ErrorException(errorMessage);
+        throw new ErrorException(errorMessage)
     }
 }
 
 var Zap = {
     /* ------------ TEXT ------------ */
-
     Messages_pre_write: function (bundle) {
-        var authentication = createAuthentication(bundle);
+        var fromNumbersArray = bundle.action_fields_full.From
+        var toNumbersArray = bundle.action_fields_full.To
+        var smsBodyArray = bundle.action_fields_full.Body
+        var smsReferenceArray = bundle.action_fields_full.Reference
+        var validityTimeArray = bundle.action_fields_full.ValidityTime
 
-        var requestHeaders = {
-            'Content-Type': 'application/json'
-        };
+        // Test if amount of fields is correct.
+        if (fromNumbersArray.length > toNumbersArray.length) {
+            throw new ErrorException("Error: there are more fields in 'From' than in 'To'. They need to be equal.")
+        } else if (toNumbersArray.length > fromNumbersArray.length) {
+            throw new ErrorException("Error: there are more fields in 'To' than in 'From'. They need to be equal.")
+        }
 
-        // Field with numbers where to send message from
-        var fromNumbersArray = bundle.action_fields_full.From;
-
-        // Field with numbers where to send message to
-        var toNumbersArray = bundle.action_fields_full.To;
-
-        // Body field/content of the message
-        var smsBodyArray = bundle.action_fields_full.Body;
-
-        // Reference field
-        var smsReferenceArray = bundle.action_fields_full.Reference;
-
-        // Validity time field
-        var validityTimeArray = bundle.action_fields_full.ValidityTime;
+        if (fromNumbersArray.length > smsBodyArray.length) {
+            throw new ErrorException("Error: there are more fields in 'From' than in 'Body'. They need to be equal.")
+        } else if (smsBodyArray.length > fromNumbersArray.length) {
+            throw new ErrorException("Error: there are more fields in 'Body' than in 'From'. They need to be equal.")
+        }
 
         // Create a list of messages
-        var messageList = [];
-        for (var j = 0; j < fromNumbersArray.length; j++) {
-            // Each message can be sent to multiple numbers
-            /* 
-            Example: 
-            Field 'To' as "number1 || number2, number3",
-            Field 'Body' as "Hello! || Hi!":
-            Number 1 receives "Hello!",
-            Number 2 and 3 receive "Hi!"
-            */
-            var numberListText = toNumbersArray[j];
-            var numberList = numberListText.split(',');
+        var messageList = []
+        for (var j = 0; j < fromNumbersArray.length; j++) { // Iterate trough all messages
+            // Each message can be send to multiple numbers
+            var numberListText = toNumbersArray[j]
+            var numberList = numberListText.split(',')
 
-            var toNumbersList = [];
-            for (var k = 0; k < numberList.length; k++) {
-                toNumbersList.push({
-                    Number: numberList[k].trim()
-                });
-            }
+            var toNumbersList = numberList.map(function (item) {
+                return {
+                    number: item.trim()
+                }
+            })
 
             messageList.push({
                 from: fromNumbersArray[j].trim(),
@@ -154,40 +196,36 @@ var Zap = {
                     type: "AUTO",
                     content: smsBodyArray[j].replace(/\r/g, "").replace(/\n/g, "").trim()
                 },
-                reference: smsReferenceArray[j] === undefined ? "None" : smsReferenceArray[j].trim(),
+                reference: smsReferenceArray[j] === undefined ? Settings.defaultReference : smsReferenceArray[j].trim(),
                 minimumNumberOfMessageParts: 1,
                 maximumNumberOfMessageParts: 8,
                 validity: parseValidityTime(validityTimeArray[j]),
                 customGrouping3: "Zapier" // Allows CM.com to track where requests originate from.
-            });
+            })
         }
-        logJSON(messageList);
 
-        var requestData = createMessagesRequestData(authentication, messageList);
-        logJSON(requestData);
-
-        return createRequest(requestHeaders, requestData);
+        var authentication = Messages.getAuthentication(bundle)
+        var requestData = Messages.getData(authentication, messageList)
+        var requestHeaders = new RequestHeaders(bundle)
+        return new ZapierRequest(requestHeaders, requestData)
     },
 
     Messages_post_write: throwResponseError,
 
-
     /* ------------ VOICE ------------ */
-
     VoiceText_pre_write: function (bundle) {
-        var main = bundle.action_fields_full.Language;
-        console.log(main);
+        var main = bundle.action_fields_full.Language
+        console.log(main)
 
-        var mainSplitted = main.split(";");
+        var mainSplitted = main.split(";")
 
-        var voiceLanguage = mainSplitted[0];
-        var voiceGender = mainSplitted[1];
-        var voiceNumber = mainSplitted[2];
+        var voiceLanguage = mainSplitted[0]
+        var voiceGender = mainSplitted[1]
+        var voiceNumber = mainSplitted[2]
 
         var requestData = {
-            callees: bundle.action_fields_full.To,
             caller: bundle.action_fields_full.From,
-            anonymous: false,
+            callees: bundle.action_fields_full.To,
             prompt: bundle.action_fields_full.Text,
             'prompt-type': "TTS",
             voice: {
@@ -195,60 +233,39 @@ var Zap = {
                 gender: voiceGender,
                 number: voiceNumber
             },
+            anonymous: false,
             validity: parseValidityTime(bundle.action_fields_full.ValidityTime)
-        };
-
-        var requestHeaders = {
-            'Content-Type': 'application/json'
-        };
-
-        if (settings.useVoiceTokenAuthentication) {
-            requestHeaders["X-CM-PRODUCTTOKEN"] = bundle.auth_fields.productToken;
-        } else {
-            var body_string = JSON.stringify(requestData);
-
-            // z.hmac(algorithm, key, string, encoding="hex")
-            var hmac_hash = z.hmac('sha256', bundle.auth_fields.shrdKey, body_string);
-
-            var user = bundle.auth_fields.userN;
-            console.log(bundle.auth_fields.shrdKey + ';' + bundle.auth_fields.userN);
-
-            requestHeaders.Authorization = 'username=' + user + ';signature=' + hmac_hash;
         }
 
-        return createRequest(requestHeaders, requestData);
+        var requestHeaders = Settings.useVoiceTokenAuthentication ? new RequestHeaders(bundle) : {
+            'Content-Type': 'application/json',
+            'Authorization': "username=" + bundle.auth_fields.userN + ";signature=" + z.hmac('sha256', bundle.auth_fields.shrdKey, JSON.stringify(requestData))
+        }
+
+        return new ZapierRequest(requestHeaders, requestData)
     },
 
     VoiceText_post_write: throwResponseError,
 
-
-    /* ------------ PAYMENTS ------------ */
-
-    // TODO
-
-
     /* ------------ NUMBER VALIDATION ------------ */
 
     Num_Validation_pre_search: function (bundle) {
-        var requestHeaders = {
-            'Content-Type': 'application/json',
-            'X-CM-PRODUCTTOKEN': bundle.auth_fields.productToken
-        };
+        var requestHeaders = new RequestHeaders(bundle)
 
         var requestData = {
             phonenumber: bundle.search_fields.PhoneNumber
-        };
+        }
 
         return {
             url: "https://api.cmtelecom.com/v1.1/numbervalidation",
             headers: requestHeaders,
             data: JSON.stringify(requestData),
             method: "POST"
-        };
+        }
     },
 
     Num_Validation_post_search: function (bundle) {
-        throwResponseError(bundle); // Stops when an error is thrown, otherwise continue below.
+        throwResponseError(bundle) // Stops when an error is thrown, otherwise continue below.
 
         return [{
             response: "Success",
@@ -256,11 +273,11 @@ var Zap = {
             status_code: bundle.response.status_code,
             content: JSON.parse(bundle.response.content),
             headers: bundle.response.headers
-        }];
+        }]
     },
 
     Num_Validation_post_read_resource: function (bundle) {
-        throwResponseError(bundle); // Stops when an error is thrown, otherwise continue below.
+        throwResponseError(bundle) // Stops when an error is thrown, otherwise continue below.
 
         return [{
             response: "Success",
@@ -268,9 +285,9 @@ var Zap = {
             status_code: bundle.response.status_code,
             content: JSON.parse(bundle.response.content),
             headers: bundle.response.headers
-        }];
+        }]
     }
-};
+}
 
 // START: FOOTER -- AUTOMATICALLY ADDED FOR COMPATIBILITY - v1.2.0
 module.exports = Zap;
