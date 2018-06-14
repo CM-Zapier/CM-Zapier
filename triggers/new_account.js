a55a7a7f47572427d32f0d39ef09e4d60455d79b
Marc <marc.j.bouwman@gmail.com>
2018-06-14 18:28:22 +0200

    Remove unneeded old files

--- triggers/new_account.js
+++ triggers/new_account.js
@@ -1,43 +1,45 @@
-// Trigger stub created by 'zapier convert'. This is just a stub - you will need to edit!
-const { replaceVars } = require('../utils');
-
 const getList = (z, bundle) => {
-  /*let url = 'https://api.cmtelecom.com/echo/v1.0/producttoken';
-  url = replaceVars(url, bundle);
-
-  const responsePromise = z.request({ url });
-  return responsePromise.then(response => {
-    response.throwForStatus();
-    return z.JSON.parse(response.content);
-  });*/
-  return {
-    status: 200,
-    content: {
-      Status: "Success"
+    // Enable when product token for voice & sms is the same.
+    /* const zapierRequest = {
+        url: "https://api.cmtelecom.com/echo/v1.0/producttoken",
+        body: {
+            // TODO
+        }
     }
-  };
+    
+    return z.request(zapierRequest).then(response => {
+        response.throwForStatus();
+        return JSON.parse(response.content);
+    }); */
+    
+    return {
+        status: 200,
+        content: {
+            Status: "Success"
+        }
+    };
 };
 
 module.exports = {
-  key: 'new_account',
-  noun: 'Account',
-
-  display: {
-    label: 'New Account',
-    description: 'Triggers when a new account is added.',
-    hidden: true,
-    important: false
-  },
-
-  operation: {
-    inputFields: [],
-    outputFields: [
-      {
-        key: 'Status',
-        type: 'string'
-      }
-    ],
-    perform: getList,
-    sample: { Status: 'Success' }
-  }
+    key: 'new_account',
+    noun: 'Account',
+    
+    display: {
+        label: 'New Account',
+        description: 'Triggers when a new account is added.',
+        hidden: true,
+        important: false
+    },
+    
+    operation: {
+        inputFields: [],
+        outputFields: [
+            {
+                key: 'Status',
+                type: 'string'
+            }
+        ],
+        perform: getList,
+        sample: { Status: 'Success' }
+    }
 };
