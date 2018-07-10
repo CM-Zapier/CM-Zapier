ef93b02ecbfa846803271e2289bf173cd276d4c0
Marc <marc.j.bouwman@gmail.com>
2018-07-10 16:45:04 +0200

    Fix voice number

--- model/Voice.js
+++ model/Voice.js
@@ -2,7 +2,7 @@ class Voice {
     constructor(language, gender, number){
         this.language = language
         this.gender = gender
-        this.number = number
+        this.number = parseInt(number)
     }
 }
 
