df9cbeb63cd99a7725defd2d1a8a6d5a322aed57
Marc <marc.j.bouwman@gmail.com>
2018-07-19 17:15:03 +0200

    Improvements for output fields

--- src/creates/contact.js
+++ src/creates/contact.js
@@ -29,9 +29,9 @@ const makeRequest = async (z, bundle) => {
     if(responseContent.customValues){
         responseContent.customValues.forEach(item => {
             if(item.fieldId === 6) responseContent.company = item.value
-            else responseContent[`customField${item.fieldId - 6}`] = item.value
+            else responseContent[`customField_${item.fieldId - 6}`] = item.value
         })
-        //delete responseContent.customValues
+        delete responseContent.customValues
     }
     
     return responseContent
