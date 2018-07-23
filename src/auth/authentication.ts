df2ee19b739e3b1db9e0157895f94ec0bed5ce64
Marc-JB <marc.j.bouwman@gmail.com>
2018-07-23 17:09:52 +0200

    Improvements

--- src/auth/authentication.ts
+++ src/auth/authentication.ts
@@ -1,7 +1,8 @@
-declare function require(path: String): any
-
 import { zObject } from "zapier-platform-core"
 import ZapierRequest from "../model/ZapierRequest"
+
+declare function require(path: string): any
+require('json5/lib/register')
 const config = require("../config.json5")
 
 const testAuthenticationValidity = async (z: zObject): Promise<object> => {
