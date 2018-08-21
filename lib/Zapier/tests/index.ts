b4b7b726934fa071d57201f4b51d3f958c7c946d
Marc <marc.j.bouwman@gmail.com>
2018-08-21 13:02:41 +0200

    Improve Zapier unit tests

--- lib/Zapier/tests/index.ts
+++ lib/Zapier/tests/index.ts
@@ -1,20 +1,13 @@
 import "should"
 import ZapierHttpRequest from "../main/ZapierHttpRequest"
 
-function objectEquals(object1: any, object2: any): boolean {
-    if(Object.keys(object1).length != Object.keys(object2).length) return false
-
-    return Object.keys(object1).every((item) => Object.keys(object2).indexOf(item) !== -1) && 
-        Object.keys(object1).every((item) => typeof object1[item] == typeof object2[item] && (typeof object1[item] == "object" ? objectEquals(object1[item], object2[item]) : object1[item] === object2[item]))
-}
-
 describe("Models", () => {
     it("ZapierRequest with url only should return correct object", () => {
         const testObject = {
             url: "https://example.com",
             method: "GET"
         }
-        objectEquals(new ZapierHttpRequest(testObject.url), testObject).should.be.true
+        new ZapierHttpRequest(testObject.url).should.have.properties(testObject)
     })
     
     it("ZapierRequest with url, POST and json body should return correct object", () => {
@@ -24,6 +17,6 @@ describe("Models", () => {
             method: "POST" as "POST",
             body: JSON.stringify(jsonBody)
         }
-        objectEquals(new ZapierHttpRequest(testObject.url, testObject.method, jsonBody), testObject).should.be.true
+        new ZapierHttpRequest(testObject.url, testObject.method, jsonBody).should.have.properties(testObject)
     })
 })
